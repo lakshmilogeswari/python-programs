0867285c1062c75e59be3b575fd0b2fd1ee5a610
lakshmilogeswari <laksmilogeswari@gmail.com>
2026-04-01 13:03:52 +0530

    Add files via upload

---
+++ DAY2.swap.py
@@ -0,0 +1,13 @@
+Python 3.15.0a7 (tags/v3.15.0a7:6024d3c, Mar 10 2026, 13:09:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+====== RESTART: C:/Users/ELCOT/Desktop/New folder (2)/day2.using python.py =====
+enternum16
+enternum26
+sum
+>>> 
+>>> 
+========== RESTART: C:/Users/ELCOT/Desktop/New folder (2)/DAY2,swap.py =========
+after swapping
+A 20
+B 10
