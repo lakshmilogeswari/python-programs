821b9d1d70b3b464e9bd3d611390fe6f216cd891
lakshmilogeswari <laksmilogeswari@gmail.com>
2026-04-01 13:02:39 +0530

    Add files via upload

---
+++ day3,add even.py
@@ -0,0 +1,6 @@
+Python 3.15.0a7 (tags/v3.15.0a7:6024d3c, Mar 10 2026, 13:09:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+= RESTART: C:/Users/ELCOT/AppData/Local/Programs/Python/Python315/new folder3/day3,odd even.py
+enter1:4
+even
