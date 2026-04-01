b5b6dc866d0e278f2110ecd59019aa9e094c4687
lakshmilogeswari <laksmilogeswari@gmail.com>
2026-04-01 13:08:31 +0530

    Add files via upload

---
+++ day3,voting out.py
@@ -0,0 +1,19 @@
+Python 3.15.0a7 (tags/v3.15.0a7:6024d3c, Mar 10 2026, 13:09:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+======== RESTART: C:/Users/ELCOT/Desktop/New folder3/day3,largest num.py =======
+7
+7
+8
+c is larlest
+>>> 
+======== RESTART: C:/Users/ELCOT/Desktop/New folder3/day3,voting inp.py ========
+20
+Traceback (most recent call last):
+  File "C:/Users/ELCOT/Desktop/New folder3/day3,voting inp.py", line 2, in <module>
+    if num>=18:
+TypeError: '>=' not supported between instances of 'str' and 'int'
+>>> 
+======== RESTART: C:/Users/ELCOT/Desktop/New folder3/day3,voting inp.py ========
+9
+not
